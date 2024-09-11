6550279cf1204d50b4749c8cdb511737fb5795e0
Fabricio Silva <fabricio1991_05@hotmail.com>
2024-09-11 15:52:49 -0300

    feat: primeiro commit

--- exercicios-backend-objetos-v3-main/src/exercicio-01/aluno.ts
+++ exercicios-backend-objetos-v3-main/src/exercicio-01/aluno.ts
@@ -2,6 +2,14 @@ type TAluno = { matricula: string; nome: string; idade: number };
 
 function solucao(matricula: string, nome: string, idade: number): TAluno {
   // seu código aqui
+
+//   const alunos = {
+//     matricula,
+//     nome,
+//     idade
+// }
+
+// return(alunos)
 }
 
 export default solucao;
