9a541c37fa1c08eaef04804c1dc76292077567bd
Fabricio Silva <fabricio1991_05@hotmail.com>
2024-09-23 21:05:56 -0300

    feat: Exericios feitos

--- exercicios-backend-tipos-utilitarios-v3-main/exercicio-03/conexao.ts
+++ exercicios-backend-tipos-utilitarios-v3-main/exercicio-03/conexao.ts
@@ -6,10 +6,20 @@ type Conn = {
     dbname: string
 }
 
-const conexao = (dados: Conn) => {
-    const { username, password, host, port, dbname } = dados;
+type Connetion = Record<'driver' | 'url', string>
+
+const conexao = (dados: Conn) : Readonly<Connetion> => {
+    const { username, password, host, port, dbname } = dados; // desestruturação
     return {
         driver: 'postgres',
         url: `postgresql://${username}:${password}@${host}:${port}/${dbname}`
     };
 }
+
+console.log(conexao ({
+    username: 'postgres',
+    password: '123456',
+    host: 'localhost',
+    port: '8000',
+    dbname: 'postgres'
+}))
