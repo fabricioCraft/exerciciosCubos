a17ae7c69a6d975d0a61b6052a2b052d46a4822f
Fabricio Silva <fabricio1991_05@hotmail.com>
2024-10-02 06:52:23 -0300

    Exercicio 2 feito

--- exercicios-backend-metodos-arrays-3-v3-main/src/exercicio-02/ordemAlfabetica.ts
+++ exercicios-backend-metodos-arrays-3-v3-main/src/exercicio-02/ordemAlfabetica.ts
@@ -1,5 +1,15 @@
 function solucao(listaAlunos: string[], ordemAlfabetica: boolean): string[] {
   // seu código aqui
+
+  if(ordemAlfabetica === true){
+    const ordenarAlunos = listaAlunos.sort((aluno1, aluno2) => {
+        return aluno1.localeCompare(aluno2)
+    })
+
+    return ordenarAlunos
+  }
+
+  return listaAlunos
   
 }
 export default solucao;
--- exercicios-backend-metodos-arrays-3-v3-main/src/index.ts
+++ exercicios-backend-metodos-arrays-3-v3-main/src/index.ts
@@ -1,37 +1,58 @@
 // Exercicio
 
-const precos: number[] = [40, 35, 9, 402, 44.1];
-const filtroPreco: string = "crescente";
+// const precos: number[] = [40, 35, 9, 402, 44.1];
+// const filtroPreco: string = "crescente";
 
-const filtrarProdutos = (precos: number[], filtroPreco: string): number[] => {
+// const filtrarProdutos = (precos: number[], filtroPreco: string): number[] => {
 
-    if (filtroPreco === 'crescente'){
-        const listaPrecos = precos.sort((preco1, preco2) =>{
-            if (preco1 > preco2){
-                return 1
-            }
+//     if (filtroPreco === 'crescente'){
+//         const listaPrecos = precos.sort((preco1, preco2) =>{
+//             if (preco1 > preco2){
+//                 return 1
+//             }
 
-            if(preco1 < preco2){
-                return -1
-            }
+//             if(preco1 < preco2){
+//                 return -1
+//             }
 
-            return 0
+//             return 0
+//         })
+//         return listaPrecos
+//     }
+
+//     const listaPrecos = precos.sort((preco1, preco2) =>{
+//         if (preco1 > preco2){
+//             return -1
+//         }
+
+//         if (preco1 < preco2){
+//             return 1
+//         }
+
+//         return 0
+//     })
+//     return listaPrecos
+// }
+
+// console.log(filtrarProdutos(precos, filtroPreco))
+
+// Exercicio 2
+
+const listaAlunos: string[] = ["Lucas", "Camila", "Taís", "Carla", "Ana"];
+const ordemAlfabetica: boolean = true;
+
+const mostrarAlunos = (listaAlunos: string[], ordemAlfabetica: boolean) => {
+
+    if(ordemAlfabetica === true){
+        const ordenarAlunos = listaAlunos.sort((aluno1, aluno2) => {
+            return aluno1.localeCompare(aluno2)
         })
-        return listaPrecos
+
+        return ordenarAlunos
     }
 
-    const listaPrecos = precos.sort((preco1, preco2) =>{
-        if (preco1 > preco2){
-            return -1
-        }
+    return listaAlunos
 
-        if (preco1 < preco2){
-            return 1
-        }
-
-        return 0
-    })
-    return listaPrecos
 }
 
-console.log(filtrarProdutos(precos, filtroPreco))
+console.log(mostrarAlunos(listaAlunos, ordemAlfabetica));
