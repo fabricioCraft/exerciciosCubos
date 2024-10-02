22d4838775539908d4318cf373ae25b035c8cf0c
Fabricio Silva <fabricio1991_05@hotmail.com>
2024-10-02 07:07:56 -0300

    feat: Exercicio 3 feito

--- exercicios-backend-metodos-arrays-3-v3-main/src/exercicio-03/imoveis.ts
+++ exercicios-backend-metodos-arrays-3-v3-main/src/exercicio-03/imoveis.ts
@@ -6,6 +6,23 @@ type TAnuncio = {
 
 function solucao(anuncios: TAnuncio[]): TAnuncio[] | string {
   // seu código aqui
-  
+
+  if (anuncios.length === 0) {
+    return "NAO ENCONTRADO";
+  }
+
+  const ordenarAnuncios = anuncios.sort((anuncio1, anuncio2) => {
+    if (anuncio1.preco > anuncio2.preco) {
+      return 1;
+    }
+
+    if (anuncio1.preco < anuncio2.preco) {
+      return -1;
+    }
+
+    return 0;
+  });
+
+  return ordenarAnuncios;
 }
 export default solucao;
--- exercicios-backend-metodos-arrays-3-v3-main/src/index.ts
+++ exercicios-backend-metodos-arrays-3-v3-main/src/index.ts
@@ -38,21 +38,86 @@
 
 // Exercicio 2
 
-const listaAlunos: string[] = ["Lucas", "Camila", "Taís", "Carla", "Ana"];
-const ordemAlfabetica: boolean = true;
+// const listaAlunos: string[] = ["Lucas", "Camila", "Taís", "Carla", "Ana"];
+// const ordemAlfabetica: boolean = true;
 
-const mostrarAlunos = (listaAlunos: string[], ordemAlfabetica: boolean) => {
+// const mostrarAlunos = (listaAlunos: string[], ordemAlfabetica: boolean) => {
 
-    if(ordemAlfabetica === true){
-        const ordenarAlunos = listaAlunos.sort((aluno1, aluno2) => {
-            return aluno1.localeCompare(aluno2)
-        })
+//     if(ordemAlfabetica === true){
+//         const ordenarAlunos = listaAlunos.sort((aluno1, aluno2) => {
+//             return aluno1.localeCompare(aluno2)
+//         })
 
-        return ordenarAlunos
-    }
+//         return ordenarAlunos
+//     }
 
-    return listaAlunos
+//     return listaAlunos
 
+// }
+
+// console.log(mostrarAlunos(listaAlunos, ordemAlfabetica));
+
+
+// Exercicio 3
+
+type TImovel = {
+    titulo: string
+    descricao: string
+    preco: number
 }
 
-console.log(mostrarAlunos(listaAlunos, ordemAlfabetica));
+const anuncios: TImovel[] = [
+    {
+      titulo: "Apartamento Reformado",
+      descricao: "Apartamento confortável e seguro",
+      preco: 3000,
+    },
+    {
+      titulo: "Casa Branca",
+      descricao: "Casa com 3 quartos e 2 banheiros",
+      preco: 1500.67,
+    },
+    {
+      titulo: "Apartamento com piscina",
+      descricao: "Apartamento confortável, com piscina, sauna e churrasqueira",
+      preco: 5600,
+    },
+    {
+      titulo: "Casa com 4 quartos",
+      descricao: "A casa possui 4 quartos, 2 banheiros e uma sala de estar",
+      preco: 2900,
+    },
+    {
+      titulo: "Kitnet",
+      descricao: "Kitnet aconchegante, com lareira e banheira privada",
+      preco: 4325,
+    },
+  ];
+
+
+  const ordenarImoveis = (anuncios: TImovel[]): TImovel[] | string => {
+
+    if(anuncios.length === 0){
+        return 'NAO ENCONTRADO'
+    }
+
+    const ordenarAnuncios = anuncios.sort((anuncio1, anuncio2) => {
+
+        if(anuncio1.preco > anuncio2.preco){
+            return 1
+        }
+
+        if(anuncio1.preco < anuncio2.preco){
+            return-1
+        }
+
+        return 0
+    })
+
+    return ordenarAnuncios
+
+
+  }
+
+  console.log(ordenarImoveis(anuncios));
+  
