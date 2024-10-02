d4e48ef7c89980dbbc80ef4b71e9a95f2a66bf8a
Fabricio Silva <fabricio1991_05@hotmail.com>
2024-10-02 07:46:08 -0300

    feat: Exercicio 4 feito

--- exercicios-backend-metodos-arrays-3-v3-main/src/exercicio-04/maioresCampeoes.ts
+++ exercicios-backend-metodos-arrays-3-v3-main/src/exercicio-04/maioresCampeoes.ts
@@ -6,6 +6,34 @@ type TTime = {
 
 function solucao(times: TTime[], paisFiltrado: string): TTime[] | string {
   // seu código aqui
+
+  
+  let encontrado = false
+   
+  for(let i =0; i < times.length; i++){
+      if(times[i].pais === paisFiltrado){
+          encontrado = true
+      }
+  }
+
+  if(!encontrado){
+      return 'NAO ENCONTRADO'
+  }
+
+  const teamsByCountry = times.sort((team1, team2) => {
+
+      if(team1.quantidadeTitulos > team2.quantidadeTitulos){
+          return -1
+      }
+
+      if(team1.quantidadeTitulos < team2.quantidadeTitulos){
+          return 1
+      }
+
+      return 0
+  })
+
+  return teamsByCountry.filter((team) => team.pais === paisFiltrado)
   
 }
 export default solucao;
--- exercicios-backend-metodos-arrays-3-v3-main/src/index.ts
+++ exercicios-backend-metodos-arrays-3-v3-main/src/index.ts
@@ -60,64 +60,160 @@
 
 // Exercicio 3
 
-type TImovel = {
-    titulo: string
-    descricao: string
-    preco: number
+// type TImovel = {
+//     titulo: string
+//     descricao: string
+//     preco: number
+// }
+
+// const anuncios: TImovel[] = [
+//     {
+//       titulo: "Apartamento Reformado",
+//       descricao: "Apartamento confortável e seguro",
+//       preco: 3000,
+//     },
+//     {
+//       titulo: "Casa Branca",
+//       descricao: "Casa com 3 quartos e 2 banheiros",
+//       preco: 1500.67,
+//     },
+//     {
+//       titulo: "Apartamento com piscina",
+//       descricao: "Apartamento confortável, com piscina, sauna e churrasqueira",
+//       preco: 5600,
+//     },
+//     {
+//       titulo: "Casa com 4 quartos",
+//       descricao: "A casa possui 4 quartos, 2 banheiros e uma sala de estar",
+//       preco: 2900,
+//     },
+//     {
+//       titulo: "Kitnet",
+//       descricao: "Kitnet aconchegante, com lareira e banheira privada",
+//       preco: 4325,
+//     },
+//   ];
+
+
+//   const ordenarImoveis = (anuncios: TImovel[]): TImovel[] | string => {
+
+//     if(anuncios.length === 0){
+//         return 'NAO ENCONTRADO'
+//     }
+
+//     const ordenarAnuncios = anuncios.sort((anuncio1, anuncio2) => {
+
+//         if(anuncio1.preco > anuncio2.preco){
+//             return 1
+//         }
+
+//         if(anuncio1.preco < anuncio2.preco){
+//             return-1
+//         }
+
+//         return 0
+//     })
+
+//     return ordenarAnuncios
+
+
+//   }
+
+//   console.log(ordenarImoveis(anuncios));
+
+
+// Exercicio 4
+
+type TTime = {
+  nome: string
+  pais: string
+  quantidadeTitulos: number
 }
 
-const anuncios: TImovel[] = [
+
+
+const times: TTime[] = [
     {
-      titulo: "Apartamento Reformado",
-      descricao: "Apartamento confortável e seguro",
-      preco: 3000,
+      nome: "Time 1",
+      pais: "Brasil",
+      quantidadeTitulos: 5,
     },
     {
-      titulo: "Casa Branca",
-      descricao: "Casa com 3 quartos e 2 banheiros",
-      preco: 1500.67,
+      nome: "Time 2",
+      pais: "Brasil",
+      quantidadeTitulos: 3,
     },
     {
-      titulo: "Apartamento com piscina",
-      descricao: "Apartamento confortável, com piscina, sauna e churrasqueira",
-      preco: 5600,
+      nome: "Time 3",
+      pais: "França",
+      quantidadeTitulos: 1,
     },
     {
-      titulo: "Casa com 4 quartos",
-      descricao: "A casa possui 4 quartos, 2 banheiros e uma sala de estar",
-      preco: 2900,
+      nome: "Time 4",
+      pais: "Espanha",
+      quantidadeTitulos: 20,
     },
     {
-      titulo: "Kitnet",
-      descricao: "Kitnet aconchegante, com lareira e banheira privada",
-      preco: 4325,
+      nome: "Time 5",
+      pais: "Espanha",
+      quantidadeTitulos: 3,
+    },
+    {
+      nome: "Time 6",
+      pais: "França",
+      quantidadeTitulos: 0,
+    },
+    {
+      nome: "Time 7",
+      pais: "Espanha",
+      quantidadeTitulos: 20,
+    },
+    {
+      nome: "Time 8",
+      pais: "Argentina",
+      quantidadeTitulos: 1,
+    },
+    {
+      nome: "Time 9",
+      pais: "França",
+      quantidadeTitulos: 9,
     },
   ];
 
+const paisFiltrado: string = "Inglaterra";
 
-  const ordenarImoveis = (anuncios: TImovel[]): TImovel[] | string => {
+const topChampionsByCountry = (times: TTime[], paisFiltrado: string): TTime[] | string => {
 
-    if(anuncios.length === 0){
-        return 'NAO ENCONTRADO'
-    }
-
-    const ordenarAnuncios = anuncios.sort((anuncio1, anuncio2) => {
-
-        if(anuncio1.preco > anuncio2.preco){
-            return 1
+        let encontrado = false
+   
+        for(let i =0; i < times.length; i++){
+            if(times[i].pais === paisFiltrado){
+                encontrado = true
+            }
         }
 
-        if(anuncio1.preco < anuncio2.preco){
-            return-1
+        if(!encontrado){
+            return 'NAO ENCONTRADO'
         }
 
-        return 0
-    })
+        const teamsByCountry = times.sort((team1, team2) => {
 
-    return ordenarAnuncios
+            if(team1.quantidadeTitulos > team2.quantidadeTitulos){
+                return -1
+            }
+
+            if(team1.quantidadeTitulos < team2.quantidadeTitulos){
+                return 1
+            }
+
+            return 0
+        })
+
+        return teamsByCountry.filter((team) => team.pais === paisFiltrado)
 
 
-  }
+}
 
-  console.log(ordenarImoveis(anuncios));
+console.log(topChampionsByCountry(times, paisFiltrado));
+
   
